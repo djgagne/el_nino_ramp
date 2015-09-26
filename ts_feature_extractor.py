aab998f41eb8c71210b2b1942d324174b18f7fd1
rudy klucik <rudyklucik@gmail.com>
2015-09-26 17:15:20 -0600

    update to ts_feature_extractor.py
    using quantiles at the top and bottom

--- ts_feature_extractor.py
+++ ts_feature_extractor.py
@@ -16,13 +16,12 @@ class FeatureExtractor(object):
         all_temps = temperatures_xray['tas'].values
         time_steps, lats, lons = all_temps.shape
         all_temps = all_temps.reshape((time_steps,lats*lons))
-        rolling_std = pd.rolling_std(pd.DataFrame(all_temps), window=12, min_periods=1).values
+        wC = 15
+        rolling_std = pd.rolling_std(pd.DataFrame(all_temps), window=wC, min_periods=1).values
         rolling_std = rolling_std[n_burn_in:-n_lookahead,:]
+        rolling_quantileHigh = pd.rolling_quantile(pd.DataFrame(all_temps), window=wC, min_periods=1, quantile=0.99).values
+        rolling_quantileHigh = rolling_quantileHigh[n_burn_in:-n_lookahead,:]
+        rolling_quantileLow = pd.rolling_quantile(pd.DataFrame(all_temps), window=wC, min_periods=1, quantile=0.01).values
+        rolling_quantileLow = rolling_quantileLow[n_burn_in:-n_lookahead,:]
         all_temps = all_temps[n_burn_in:-n_lookahead,:]
-        all_diffs = np.zeros_like(all_temps)
-        all_diffs[1:-1,:] = all_temps[:-2,:] - all_temps[2:,:]
-        all_diffs[1,:] = all_temps[0,:] - all_temps[1,:]
-        all_diffs[-1,:] = all_temps[-2,:] - all_temps[-1,:]
-        print("Diffs shape {0:d},{1:d}".format(*all_diffs.shape))
-        print("Std shape {0:d},{1:d}".format(*rolling_std.shape))
-        return np.hstack((all_temps, all_diffs, rolling_std))
+        return np.hstack((all_temps, rolling_std, rolling_quantileHigh, rolling_quantileLow))
