a0214baf4fa3a05fed30f43a98c8d7e254d125f7
David John Gagne II <djgagne@ou.edu>
2015-09-26 14:21:24 -0600

    Added time diffs and a big box subset

--- regressor.py
+++ regressor.py
@@ -1,6 +1,7 @@
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.base import BaseEstimator
 
+
 class Regressor(BaseEstimator):
     def __init__(self):
         self.clf = GradientBoostingRegressor(n_estimators=200, max_features="sqrt", max_depth=5)
--- ts_feature_extractor.py
+++ ts_feature_extractor.py
@@ -1,4 +1,6 @@
+import numpy as np
 class FeatureExtractor(object):
+
     def __init__(self):
         pass
 
@@ -8,8 +10,18 @@ class FeatureExtractor(object):
     def transform(self, temperatures_xray, n_burn_in, n_lookahead, skf_is):
         """Use world temps as features."""
         # Set all temps on world map as features
-        all_temps = temperatures_xray['tas'].values
+        lat_bottom = -80
+        lat_top = 80
+        lon_left = 80
+        lon_right = 320
+        #all_temps = temperatures_xray['tas'].values
+        all_temps = temperatures_xray['tas'].loc[:, lat_bottom:lat_top, lon_left:lon_right].values
+        print all_temps.shape
         time_steps, lats, lons = all_temps.shape
-        all_temps = all_temps.reshape((time_steps, lats * lons))
-        all_temps = all_temps[n_burn_in:-n_lookahead, :]
-        return all_temps
+        all_temps = all_temps.reshape((time_steps,lats*lons))
+        all_temps = all_temps[n_burn_in:-n_lookahead,:]
+        all_diffs = np.zeros_like(all_temps)
+        all_diffs[1:-1,:] = all_temps[:-2,:] - all_temps[2:,:]
+        all_diffs[1,:] = all_temps[0,:] - all_temps[1,:]
+        all_diffs[-1,:] = all_temps[-2,:] - all_temps[-1,:]
+        return np.hstack((all_temps, all_diffs))
